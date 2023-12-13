74036d479a9df7b76a87f8a7daaf2e271ace257a
Christian LeVesque <christian@levesque.dev>
2023-12-13 12:58:21 -0600

    saving work again

--- WifleTools.Blazor/Program.cs
+++ WifleTools.Blazor/Program.cs
@@ -1,31 +1,24 @@
-using System;
-using System.Diagnostics;
-using System.IO;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MudBlazor.Services;
 using QuestPDF.Infrastructure;
 using WifleTools.Data;
 using WifleTools.Extensions;
-using WifleTools.Files;
 using WifleTools.Infrastructure;
 using WifleTools.Pdf;
 using WifleTools.State;
+using WifleTools.Tools;
 
 namespace WifleTools;
 
 public static class Program
 {
-	public static async Task Main(string[] args)
+	public static void Main(string[] args)
 	{
 		QuestPDF.Settings.License = LicenseType.Community;
 
-		var fileWriter = new FileWriter();
-		Initialize(fileWriter);
+		StartupUtils.EnsureAppdataDirectoryExists();
 
 		var builder = WebApplication.CreateBuilder(args);
 
@@ -36,17 +29,16 @@ public static class Program
 		builder.Services
 			.AddMudServices()
 			.AddDbContext<AppDbContext>(
-				o => o.UseWifleDb(fileWriter),
+				o => o.UseWifleDb(),
 				ServiceLifetime.Transient)
 			.AddSingleton<LayoutState>()
 			.AddTransient(typeof(ICrudService<>), typeof(CrudService<>))
 			.AddTransient<InvoicePdfGenerator>()
-			.AddTransient(typeof(IStatusLogger<>), typeof(StatusLogger<>))
-			.AddSingleton<IFileWriter>(fileWriter);
+			.AddTransient(typeof(IStatusLogger<>), typeof(StatusLogger<>));
 
 		var app = builder.Build();
 
-		await MigrateDb(app.Services, fileWriter);
+		app.Services.MigrateDb();
 
 		// Configure the HTTP request pipeline.
 		if (!app.Environment.IsDevelopment())
@@ -61,15 +53,6 @@ public static class Program
 		app.MapBlazorHub();
 		app.MapFallbackToPage("/_Host");
 
-		await app.RunAsync();
-	}
-
-	private static void Initialize(IFileWriter fileWriter)
-	{
-		Debug.WriteLine($"Folder is {fileWriter.BaseDirectory}");
-		if (!Directory.Exists(fileWriter.BaseDirectory))
-		{
-			Directory.CreateDirectory(fileWriter.BaseDirectory);
-		}
+		app.Run();
 	}
 }
